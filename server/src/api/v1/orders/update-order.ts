c4b602a8ad7cc2e3b96f74650a261858a73eaa6a
Dominik Ruf <aquanik.ruf@gmail.com>
2020-11-18 08:46:32 +0100

    FEATURE: added logic to update-order

--- server/src/api/v1/orders/update-order.ts
+++ server/src/api/v1/orders/update-order.ts
@@ -1,8 +1,40 @@
 import { Request, Response } from "express";
+import isBlank from "is-blank";
+import { mapPlan } from "../../../functions/map-plan.func";
 import { wrapResponse } from "../../../functions/response-wrapper";
+import { InternalOrder } from "../../../interfaces/orders.interface";
+import { InternalPlan } from "../../../interfaces/plan.interface";
+import { Order } from "../../../models/order.model";
 
 export async function updateOrder(req: Request, res: Response) {
-    
+    let success = true;
+    let data;
+    try {
+        if (isBlank(req.body) || req.params.id === null) {
+            throw 'No body or valid param set.';
+        }
+        await Order.findOne(
+            {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then((foundOrder) => data = foundOrder)
+            .catch(error => {
+                    success = false;
+                    data = null;
+                }
+            );
+    } catch (e) {
+        res.send(wrapResponse(success, {error: e}));
+        return;
+    }
+    //Order Objekt from database must not be null, to change it.
+    if(data !== null){
+        //TODO: params finden, die gesetzt sind, params neu setzen, neuen Datensatz zurückgeben.
+    }else{
+        success = false;
+    }
 
-    return res.send(wrapResponse(true));
+    return res.send(wrapResponse(success, {data: data}));
 }
