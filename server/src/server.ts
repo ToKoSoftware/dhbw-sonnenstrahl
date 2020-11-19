6d8a0b08fd25aa4a9dcbeb4c75673e7bca477978
Tomas Kostadinov <github@kostadinov.it>
2020-11-19 21:40:17 +0100

    * Rename route to match convention * Show error when record is not found (this needs to be refined)

--- server/src/server.ts
+++ server/src/server.ts
@@ -52,7 +52,7 @@ export default function startServer() {
     app.get('/api/v1/orders/:id', (req, res) => getOrder(req, res));
     app.post('/api/v1/orders', (req, res) => createOrder(req, res));
     app.post('/orders', (req, res) => createOrder(req, res));
-    app.delete('/api/v1/order/:id', (req, res) => deleteOrder(req, res));
+    app.delete('/api/v1/orders/:id', (req, res) => deleteOrder(req, res));
 
     app.use((req, res, next) => {
         res.status(404).send(wrapResponse(false, {
