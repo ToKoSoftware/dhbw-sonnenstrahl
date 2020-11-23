e30202ad967bdb044cab80d3f2e2b9b291650644
LisaFremdt <lisa.fremdt@t-online.de>
2020-11-23 12:54:41 +0100

    added EmailValidator and added in create-User a respective if-code

--- server/src/api/v1/users/create-user.ts
+++ server/src/api/v1/users/create-user.ts
@@ -5,6 +5,7 @@ import {User} from '../../../models/user.model';
 import {mapUser} from '../../../functions/map-users.func';
 import {objectHasRequiredAndNotEmptyKeys} from '../../../functions/checkInputs.func';
 import { Vars } from '../../../vars';
+import * as EmailValidator from 'email-validator';
 
 export async function createUser(req: Request, res: Response) {
     const incomingData: IncomingUser = req.body;
@@ -15,24 +16,29 @@ export async function createUser(req: Request, res: Response) {
         res.send(wrapResponse(false, {error: 'Not all required fields have been set'}));
         return;
     }
+    let validEmail = EmailValidator.validate(mappedIncomingData.email);
 
-    let user = await User.findOne(
-        {
-            where: {
-                email: mappedIncomingData.email
-            }
-        }
-    ).catch((error) => {
-        return null;
-    });
-
-    if (user === null){
-        let data = await User.create(mappedIncomingData).then((res) => res).catch(error => null);
-        if (data === null) {
-            return res.send(wrapResponse(false, {error: 'Could not create User'}));
-        }
-        return res.send(wrapResponse(true, data));
+    if (validEmail === false) {
+        return res.send(wrapResponse(false, {error: 'E-mail is not valid'}));
     } else {
-        return res.send(wrapResponse(false, {error: 'E-mail is already in use'}));
+        let user = await User.findOne(
+            {
+                where: {
+                    email: mappedIncomingData.email
+                }
+            }
+        ).catch((error) => {
+            return null;
+        });
+
+        if (user === null){
+            let data = await User.create(mappedIncomingData).then((res) => res).catch(error => null);
+            if (data === null) {
+                return res.send(wrapResponse(false, {error: 'Could not create User'}));
+            }
+            return res.send(wrapResponse(true, data));
+        } else {
+            return res.send(wrapResponse(false, {error: 'E-mail is already in use'}));
+        }
     }
 }
