f3f06fb8b5b27a85743750be1ba6c2a38c1856e4
Dominik Ruf <aquanik.ruf@gmail.com>
2020-11-25 13:43:06 +0100

    FIX: delete customer

--- server/src/api/v1/customers/delete-customer.ts
+++ server/src/api/v1/customers/delete-customer.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
-import { Op } from "sequelize/types";
 import { wrapResponse } from "../../../functions/response-wrapper";
 import { Customer } from "../../../models/customer.models";
 import { Order } from "../../../models/order.model";
+import { Op } from "sequelize"
+
 
 export async function deleteCustomer(req: Request, res: Response) {
     // Can only set Customer to inactive, if none of his orders is active/not terminated
