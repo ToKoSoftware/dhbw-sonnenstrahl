c1b0ff4b93f5cb425f02fa8bb8d819fc048e8691
Dominik Ruf <aquanik.ruf@gmail.com>
2020-11-25 14:57:43 +0100

    FEATURE: add create and update customer

--- server/src/api/v1/customers/update-customer.ts
+++ server/src/api/v1/customers/update-customer.ts
@@ -1,5 +1,72 @@
 import { Request, Response } from "express";
+import isBlank from "is-blank";
+import { checkKeysAreNotEmptyOrNotSet } from "../../../functions/check-inputs.func";
+import { mapCustomer } from "../../../functions/map-customer.func";
+import { wrapResponse } from "../../../functions/response-wrapper";
+import { IncomingCustomer, InternalCustomer } from "../../../interfaces/customers.interface";
+import { Customer } from "../../../models/customer.models";
 
 export async function updateCustomer(req: Request, res: Response) {
+
+    let d;
+    const incomingData: IncomingCustomer = req.body;
+    const mappedIncomingData: InternalCustomer = mapCustomer(incomingData);
+
+    let requiredFields = Customer.requiredFields();
+
+    if (isBlank(req.body) || req.params.id === null) {
+        return res.send(wrapResponse(false, { error: "No body or valid param set." }));
+
+    } else {
+        d = await Customer.findOne(
+            {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .catch(error => {
+                d = null;
+            });
+    }
+
+    //Customer Objekt from database must not be null, to change it.
+    if (d !== null && (req.body.id === undefined || req.params.id === req.body.id) &&  checkKeysAreNotEmptyOrNotSet(mappedIncomingData, requiredFields) !== false){
+        // TODO Check, ob der zipCode in der IncomingOrder überhaupt der, des entsprechenden Plans ist
+        d = await Customer.update(
+            req.body, 
+            {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .catch(error => {
+                return res.send(wrapResponse(false, { error: "Update failed." }));
+            });
+
+    } else if (d === null) {
+        return res.send(wrapResponse(false, { error: "No order with given id found" }));
+
+    } else if(checkKeysAreNotEmptyOrNotSet(mappedIncomingData, requiredFields) === false) {
+        return res.send(wrapResponse(false, { error: "Fields must not be empty" }));
+
+    } else if(req.body.id !== undefined || req.params.id !== req.body.id) {
+        return res.send(wrapResponse(false, { error: "ID must not be changed" }));
+    } else {
+        return res.send(wrapResponse(false));
+    }
+
+    let success = true;
+    d = await Customer.findOne(
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .catch(error => {
+            success = false;
+            d = null;
+        });
+
+    return res.send(wrapResponse(success, { data: d }));
     
 }
