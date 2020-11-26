afc43f82280731604ee3b81f1d4d8c4bc15128de
Dominik Ruf <aquanik.ruf@gmail.com>
2020-11-26 21:01:18 +0100

    FIX: Customer was created, if plan.postcode and customer.postcode of incoming order did not match

--- server/src/api/v1/orders/create-order.ts
+++ server/src/api/v1/orders/create-order.ts
@@ -11,9 +11,32 @@ import { mapCustomer } from '../../../functions/map-customer.func';
 
 export async function createOrder(req: Request, res: Response) {
     const incomingData: IncomingOrder = req.body;
+    const mappedCustomerData = mapCustomer(incomingData);
+    
     //Check, if Customer with given params already exists. If not create one.
     let customerId = "";
     let err = false;
+
+    // Try to find Plan with given planId
+    let plan: Plan | null = await Plan.findOne(
+        {
+            where: {
+                id: incomingData.rateId,
+                is_active: true
+            }
+        }
+    ).catch((error) => {
+        return null;
+    });
+    if (plan === null) {
+        return res.status(404).send(wrapResponse(false, {error: 'Plan cannot be found'}));
+    }
+
+
+    // Postcode of plan and order must match
+    if(plan.postcode != mappedCustomerData.postcode){
+        return res.status(400).send(wrapResponse(false, {error: 'Postcode of plan and order do not match!'}));
+    }
     let customer: Customer | null = await Customer.findOne(
         {
             where: {
@@ -35,43 +58,17 @@ export async function createOrder(req: Request, res: Response) {
     }
     if(customer === null){
         // Customer not found. Create new!
-        let mappedCustomerData = mapCustomer(incomingData);
-        let requiredFields = Customer.requiredFields();
-        if (!objectHasRequiredAndNotEmptyKeys(mappedCustomerData, requiredFields)) {
-            return res.status(400).send(wrapResponse(false, {error: 'Not all required fields have been set'}));
-        }
         customer = await Customer.create(mappedCustomerData).catch((error) => {err = true; return null;});
     }
     if(err || customer === null){
         return res.status(500).send(wrapResponse(false, {error: 'Database error'}));
     }
-
-    const mappedIncomingData = mapOrder(incomingData, customer.id);
         
+    const mappedIncomingData = mapOrder(incomingData, customer.id);
     // Check, if all required fields have been set
     let requiredFields = Order.requiredFields();
     if (!objectHasRequiredAndNotEmptyKeys(mappedIncomingData, requiredFields)) {
         return res.status(400).send(wrapResponse(false, {error: 'Not all required fields have been set'}));
-    }
-
-    // Try to find Plan with given planId
-    let plan: Plan | null = await Plan.findOne(
-        {
-            where: {
-                id: mappedIncomingData.planId,
-                is_active: true
-            }
-        }
-    ).catch((error) => {
-        return null;
-    });
-    if (plan === null) {
-        return res.status(404).send(wrapResponse(false, {error: 'Plan cannot be found'}));
-    }
-
-    // Postcode of plan and order must match
-    if(plan.postcode != customer.postcode){
-        return res.status(400).send(wrapResponse(false, {error: 'Postcode of plan and order do not match!'}));
     }
 
     // Create order
