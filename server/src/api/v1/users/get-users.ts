180b5c3ebd22131c3dc9c4d1225afe965c2bcf76
Dominik Ruf <aquanik.ruf@gmail.com>
2020-12-01 22:49:02 +0100

    FEATURE: authing in getUser and updateUser

--- server/src/api/v1/users/get-users.ts
+++ server/src/api/v1/users/get-users.ts
@@ -3,9 +3,15 @@ import { User } from '../../../models/user.model';
 import { wrapResponse } from '../../../functions/response-wrapper';
 import { FindOptions } from 'sequelize';
 import { buildQuery, customFilterValueResolver, QueryBuilderConfig } from '../../../functions/query-builder.func';
+import { currentUserIsAdminOrMatchesId } from '../../../functions/current-user-is-admin-or-matches-id.func';
 
 export async function getUser(req: Request, res: Response) {
     let success = true;
+
+    if (!currentUserIsAdminOrMatchesId(req.params.id)) {
+        return res.status(403).send(wrapResponse(false, { error: 'Unauthorized!' }));
+    }
+
     let data = await User.findOne(
         {
             where: {
