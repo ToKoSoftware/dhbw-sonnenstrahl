43e945ced573cfb95d6d58507de9ee339736604d
Tomas Kostadinov <github@kostadinov.it>
2020-12-04 23:03:27 +0100

    * Show admin option only when user is admin

--- frontend/src/app/services/login/login.service.ts
+++ frontend/src/app/services/login/login.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ReplaySubject} from 'rxjs';
 import jwt_decode from 'jwt-decode';
+import {ApiService} from '../api/api.service';
+import {UserData} from '../../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,33 +10,41 @@ import jwt_decode from 'jwt-decode';
 export class LoginService {
   public isLoggedIn$: ReplaySubject<boolean> = new ReplaySubject();
   public isAdmin$: ReplaySubject<boolean> = new ReplaySubject();
-  public currentUser$: ReplaySubject<CurrentUser> = new ReplaySubject();
+  public currentUser$: ReplaySubject<UserData | null> = new ReplaySubject();
   public jwt$: ReplaySubject<string> = new ReplaySubject();
 
   constructor() {
+    this.reloadJWT();
+  }
+
+  public login(jwt: string): void {
+    localStorage.setItem('jwt', jwt);
+    this.reloadJWT();
+  }
+
+  public logout(): void {
+    localStorage.removeItem('jwt');
+    this.reloadJWT();
+  }
+
+  private reloadJWT(): void {
+    this.currentUser$.next(null);
     const jwt: string | null = localStorage.getItem('jwt');
     try {
-      const decodedJWT = jwt_decode(jwt || '');
+      const decodedJWT = jwt_decode<JWT>(jwt || '');
       this.isLoggedIn$.next(true);
-      console.log(decodedJWT);
+      this.isAdmin$.next(decodedJWT.is_admin);
     } catch (error) {
       this.isLoggedIn$.next(false);
     }
   }
 
-  public login(jwt: string): void {
-    localStorage.setItem('jwt', jwt);
-    this.isLoggedIn$.next(true);
-  }
-
-  public logout(): void {
-    localStorage.removeItem('jwt');
-    this.isLoggedIn$.next(false);
-  }
 }
 
-export interface CurrentUser {
+export interface JWT {
   email: string;
   id: string;
   is_admin: boolean;
+  iat: number;
+  exp: number;
 }
