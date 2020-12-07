ed725161af7a72810d9c5a87e69c6102ebd9280f
Tomas Kostadinov <github@kostadinov.it>
2020-12-07 21:42:42 +0100

    * Implemented editing of customers * Updated frontend interfaces

---
+++ frontend/src/app/interfaces/customer.interface.ts
@@ -0,0 +1,12 @@
+export interface CustomerData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  street: string;
+  streetNumber: string;
+  postcode: string;
+  city: string;
+  is_active: string;
+  createdAt: string;
+  updatedAt: string;
+}
