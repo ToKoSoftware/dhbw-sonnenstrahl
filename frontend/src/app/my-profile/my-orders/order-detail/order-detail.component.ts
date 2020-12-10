c6aabe52b9d9e3290b8982c140e688ed722ee274
Tomas Kostadinov <github@kostadinov.it>
2020-12-10 15:47:12 +0100

    * Implemented frontend for order termination

--- frontend/src/app/my-profile/my-orders/order-detail/order-detail.component.ts
+++ frontend/src/app/my-profile/my-orders/order-detail/order-detail.component.ts
@@ -6,6 +6,7 @@ import {ModalService} from '../../../services/modal/modal.service';
 import {ApiService} from '../../../services/api/api.service';
 import {PlanData} from '../../../interfaces/plan.interface';
 import {CustomerData} from '../../../interfaces/customer.interface';
+import {UserData} from '../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-order-detail',
@@ -27,6 +28,39 @@ export class OrderDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  public async cancelOrder(): Promise<void> {
+    const confirmed = await this.confirmService.confirm({
+      title: `Sicher, dass Sie den diesen Tarif kündigen möchten?`,
+      description: 'Dies kann nicht rückgängig gemacht werden.'
+    });
+    if (confirmed) {
+      this.loadingService.showLoading();
+      this.api.put<{ success: boolean } | { success: boolean, error: string }>(`/orders/${this.order.id}/terminate`).subscribe(
+        data => {
+          this.order.terminatedAt = new Date().toISOString();
+          this.loadingService.hideLoading();
+        },
+        error => {
+          this.loadingService.hideLoading();
+          this.confirmService.confirm({
+            title: `Es ist ein Fehler beim Kündigen aufgetreten.`,
+            confirmButtonType: 'info',
+            confirmText: 'Ok',
+            description: 'Der Server gab folgenden Fehler an: ' + error.error.data.error,
+            showCancelButton: false
+          });
+        }
+      );
+    }
+  }
+
+
+  private loadData(): void {
+    this.loadingCustomer = true;
+    this.loadingPlan = true;
     this.api.get<PlanData>(`/plans/${this.order.planId}`).subscribe(
       data => {
         this.plan = data.data;
@@ -40,5 +74,4 @@ export class OrderDetailComponent implements OnInit {
       }
     );
   }
-
 }
