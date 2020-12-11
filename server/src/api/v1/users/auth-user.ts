771a91d1134e1393b555bd0dc54b0413bd2c3c1c
Tomas Kostadinov <github@kostadinov.it>
2020-12-11 17:27:56 +0100

    * Implemented password hashing

--- server/src/api/v1/users/auth-user.ts
+++ server/src/api/v1/users/auth-user.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from "express";
-import { wrapResponse } from "../../../functions/response-wrapper";
-import { IncomingUser, InternalUser } from "../../../interfaces/users.interface";
-import { mapUser } from '../../../functions/map-users.func';
-import { User } from "../../../models/user.model";
-import jwt from "jsonwebtoken";
-import { Vars } from "../../../vars";
+import {Request, Response} from 'express';
+import {wrapResponse} from '../../../functions/response-wrapper';
+import {IncomingUser, InternalUser} from '../../../interfaces/users.interface';
+import {mapUser} from '../../../functions/map-users.func';
+import {User} from '../../../models/user.model';
+import jwt from 'jsonwebtoken';
+import {Vars} from '../../../vars';
 import * as bcrypt from 'bcryptjs';
 
 export async function loginUser(req: Request, res: Response) {
-   
+
     const incomingData: IncomingUser = req.body;
     const mappedIncomingData: InternalUser = await mapUser(incomingData);
 
     let success = true;
-    let calculatedExpiresIn =  60*60; //expiration after 1h
+    let calculatedExpiresIn = 60 * 60; //expiration after 1h
 
 
     const user = await User.findOne(
@@ -31,21 +31,28 @@ export async function loginUser(req: Request, res: Response) {
 
 
     if (!success) {
-        res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+        res.status(500).send(wrapResponse(false, {error: 'Database error'}));
     }
 
     if (user === null) {
-        res.status(403).send(wrapResponse(false, { error: 'Unauthorized' }));
+        res.status(403).send(wrapResponse(false, {error: 'Unauthorized'}));
     } else {
-       /*
-        await bcrypt.compare(req.body.password, user.password).then((isMatch: any) => {if (!isMatch) return res.status(400).send(wrapResponse(false, { error: 'Unauthorized!' }))})
-        .catch(error => {
-            success = false;
-            return null;
-        });
-        */
-        const token = jwt.sign({ id: user.id, email: user.email, is_admin: user.is_admin}, Vars.config.database.jwtSalt, { expiresIn: calculatedExpiresIn });
-            return res.send(wrapResponse(true, token))
-        };
+        const passwordMatches = await bcrypt.compare(req.body.password, user.password)
+            .then(matches => matches).catch(error => {
+                return false;
+            });
+        if (passwordMatches) {
+            const token = jwt.sign({
+                    id: user.id,
+                    email: user.email,
+                    is_admin: user.is_admin
+                },
+                Vars.config.database.jwtSalt,
+                {expiresIn: calculatedExpiresIn}
+            );
+            return res.send(wrapResponse(true, token));
+        }
+        return res.status(403).send(wrapResponse(false, {error: 'Unauthorized!'}));
     }
+}
 
