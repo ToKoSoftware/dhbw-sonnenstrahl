73a1f85b41f97bb573295309c344d1b23902ef10
Dominik Ruf <aquanik.ruf@gmail.com>
2020-12-21 12:34:42 +0100

    FEATURE: renaming colums

--- server/src/api/v1/plans/get-plans.ts
+++ server/src/api/v1/plans/get-plans.ts
@@ -1,8 +1,9 @@
 import {Plan} from '../../../models/plan.model';
 import {Request, Response} from 'express';
-import {FindOptions} from 'sequelize';
+import {FindOptions, Sequelize, QueryTypes} from 'sequelize';
 import {wrapResponse} from '../../../functions/response-wrapper';
 import {buildQuery, customFilterValueResolver, QueryBuilderConfig} from '../../../functions/query-builder.func';
+import { Vars } from '../../../vars';
 
 export async function getPlans(req: Request, res: Response): Promise<Response> {
     let query: FindOptions = {
@@ -37,32 +38,30 @@ export async function getPlans(req: Request, res: Response): Promise<Response> {
 }
 
 export async function getPlansInExternalFormat(req: Request, res: Response): Promise<Response> {
-    let query: FindOptions = {
-        raw: true,
-    };
-    // todo move this to the model
-    const allowedSearchFields = ['plan', 'postcode'];
-    const allowedOrderFields = ['postcode', 'plan', 'cost_var', 'cost_fix'];
-    const allowedFilterFields = ['id', 'postcode', 'plan', 'is_active'];
-
-    const customResolver = new Map<string, customFilterValueResolver>();
-    customResolver.set('is_active', (field: string, requ: Request, value: string) => {
-        return true;
+    const incomingZipCode = req.query.zipCode as string;
+    const zipCodeAsNumber = parseInt(incomingZipCode);
+    const incomingConsumption = parseInt(req.query.consumption as string);
+    Vars.loggy.log(incomingConsumption);
+    if( zipCodeAsNumber > 99999 || isNaN(zipCodeAsNumber) || incomingConsumption * zipCodeAsNumber <= 0 || isNaN(incomingConsumption)){
+        return res.status(400).send(wrapResponse(false, {error: 'Not all required fields set or wrong data.'}));
+    }
+    // calculatedCosts = Math.round((plan.cost_var / 10000 * incomingData.consumption + plan.cost_fix / 10000 + Number.EPSILON) * 100) / 100;
+    const data: Plan[] = await Plan.findAll({
+        attributes: [
+            'id', 
+            ['plan', 'title'], 
+            ['postcode', 'zipCode'], 
+            ['cost_var', 'pricePerUnit'], 
+            ['cost_fix', 'basicPrice']
+        ], 
+        where: {
+            postcode: incomingZipCode
+        },
+        raw: true
     });
-    const inputSearchString = req.query.search as string || '';
-    const queryConfig: QueryBuilderConfig = {
-        query: query,
-        searchString: inputSearchString.replace('zipCode', 'postcode'),
-        customFilterResolver: customResolver,
-        allowLimitAndOffset: true,
-        allowedFilterFields: allowedFilterFields,
-        allowedSearchFields: allowedSearchFields,
-        allowedOrderFields: allowedOrderFields
-    };
-    query = buildQuery(queryConfig, req);
-    const data: Plan[] = await Plan.findAll(query);
-    //TODO formatting of output
+    //TODO costs in array
     return res.send(wrapResponse(true, data));
+    
 }
 
 export async function getPlan(req: Request, res: Response): Promise<Response> {
