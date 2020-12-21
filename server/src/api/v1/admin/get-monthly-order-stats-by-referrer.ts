c79885e6176347627d49f769b7fbc105ccfe2d93
Dominik Ruf <aquanik.ruf@gmail.com>
2020-12-21 15:09:41 +0100

    FIX: delete unnecessary import

--- server/src/api/v1/admin/get-monthly-order-stats-by-referrer.ts
+++ server/src/api/v1/admin/get-monthly-order-stats-by-referrer.ts
@@ -2,7 +2,6 @@ import {Request, Response} from 'express';
 import {Sequelize} from 'sequelize-typescript';
 import {wrapResponse} from '../../../functions/response-wrapper';
 import {Order} from '../../../models/order.model';
-import { Vars } from '../../../vars';
 
 export async function getMonthlOrderStatsByReferrer(req: Request, res:Response): Promise<Response> {
     let success = true;
