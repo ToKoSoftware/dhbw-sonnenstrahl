b90a24eedab2b9a7182c524826be40888520f761
Dominik Ruf <aquanik.ruf@gmail.com>
2020-12-22 10:48:01 +0100

    REFACTOR: move getPlansInExternalFormat to own file

--- server/src/api/v1/plans/get-plans.ts
+++ server/src/api/v1/plans/get-plans.ts
@@ -37,46 +37,6 @@ export async function getPlans(req: Request, res: Response): Promise<Response> {
     return res.send(wrapResponse(true, data));
 }
 
-export async function getPlansInExternalFormat(req: Request, res: Response): Promise<Response> {
-    const incomingZipCode = req.query.zipCode as string;
-    const zipCodeAsNumber = parseInt(incomingZipCode);
-    const incomingConsumption = parseInt(req.query.consumption as string);
-    if( zipCodeAsNumber > 99999 || isNaN(zipCodeAsNumber) || incomingConsumption * zipCodeAsNumber <= 0 || isNaN(incomingConsumption)){
-        return res.status(400).send(wrapResponse(false, {error: 'Not all required fields set or wrong data.'}));
-    }
-    const plans = await Plan.findAll({
-        attributes: [
-            'id', 
-            'plan', 
-            'postcode', 
-            'cost_var', 
-            'cost_fix'
-        ],
-        where: {
-            postcode: incomingZipCode
-        },
-        raw: true
-    });
-    const outputData: ExternalPlanData[] = [];
-    plans.forEach(element => {
-        const calculatedCosts = Math.round((element.cost_var / 10000 * incomingConsumption + element.cost_fix / 10000 + Number.EPSILON) * 100) / 100;
-        outputData.push({
-            id: element.id,
-            title: element.plan,
-            zipCode: element.postcode,
-            pricePerUnit: element.cost_var,
-            basicPrice: element.cost_fix,
-            consumption: incomingConsumption,
-            calculatedPricePerYear: calculatedCosts
-        });
-    });
-    outputData.sort(function(a, b){
-        return a.calculatedPricePerYear - b.calculatedPricePerYear;
-    });
-    return res.send(wrapResponse(true, outputData));
-    
-}
-
 export async function getPlan(req: Request, res: Response): Promise<Response> {
     let success = true;
     const data = await Plan.findOne(
