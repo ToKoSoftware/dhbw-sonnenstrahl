6d01c7bdb64439c91a71d1e183e241d4ee0f85a8
LisaFremdt <lisa.fremdt@t-online.de>
2020-12-23 19:26:05 +0100

    formatting

--- server/src/seeders/20201217175111-demo-cutomers-orders.js
+++ server/src/seeders/20201217175111-demo-cutomers-orders.js
@@ -32,99 +32,95 @@ module.exports = {
         let customers =[];
         let orders = [];
         let referrers = ['VERIVOX', 'CHECK24', 'switchup', 'Stromvergleich', 'Wechselpiraten'];
-        
+
         await queryInterface.sequelize.query('SELECT id, postcode FROM "Plans";', { type: queryInterface.sequelize.QueryTypes.SELECT })
-        .then(async function(plans) {
+            .then(async function(plans) {
 
-            //200 Customer, die zu User gehören
-            for (let i = 0; i <= 199; i++) {
-                customers.push({
-                    id: v4(),
-                    firstName: faker.name.firstName(),
-                    lastName: faker.name.lastName(),
-                    street: faker.address.streetName(),
-                    streetNumber: faker.random.number(),
-                    postcode: plans[i].postcode,
-                    city: faker.address.city(),
-                    createdAt: users[i].createdAt,
-                    updatedAt: timeFunc.randomTime(users[i].createdAt, timeFunc.endTime),
-                    userId: users[i].id,
-                    is_active: true
-                });
-            }
+                //200 Customer, die zu User gehören
+                for (let i = 0; i <= 199; i++) {
+                    customers.push({
+                        id: v4(),
+                        firstName: faker.name.firstName(),
+                        lastName: faker.name.lastName(),
+                        street: faker.address.streetName(),
+                        streetNumber: faker.random.number(),
+                        postcode: plans[i].postcode,
+                        city: faker.address.city(),
+                        createdAt: users[i].createdAt,
+                        updatedAt: timeFunc.randomTime(users[i].createdAt, timeFunc.endTime),
+                        userId: users[i].id,
+                        is_active: true
+                    });
+                }
 
-            //50 Customer, die zu User gehören, die schon einen Customer haben
-            for (let i = 0; i <= 49; i++) {
-                const c = timeFunc.randomTime(users[i].createdAt, timeFunc.endTime);
-                customers.push({
-                    id: v4(),
-                    firstName: faker.name.firstName(),
-                    lastName: faker.name.lastName(),
-                    street: faker.address.streetName(),
-                    streetNumber: faker.random.number(),
-                    postcode: plans[i+200].postcode,
-                    city: faker.address.city(),
-                    createdAt: c,
-                    updatedAt: timeFunc.randomTime(c, timeFunc.endTime),
-                    userId: users[i].id,
-                    is_active: true,
-                });
-            }
+                //50 Customer, die zu User gehören, die schon einen Customer haben
+                for (let i = 0; i <= 49; i++) {
+                    const c = timeFunc.randomTime(users[i].createdAt, timeFunc.endTime);
+                    customers.push({
+                        id: v4(),
+                        firstName: faker.name.firstName(),
+                        lastName: faker.name.lastName(),
+                        street: faker.address.streetName(),
+                        streetNumber: faker.random.number(),
+                        postcode: plans[i+200].postcode,
+                        city: faker.address.city(),
+                        createdAt: c,
+                        updatedAt: timeFunc.randomTime(c, timeFunc.endTime),
+                        userId: users[i].id,
+                        is_active: true,
+                    });
+                }
 
-            await queryInterface.bulkInsert('Customers', customers);
+                await queryInterface.bulkInsert('Customers', customers);
 
-        //250 Customer (175 (70%) haben eine Order)
+                //250 Customer (175 (70%) haben eine Order)
+                for (let i = 0; i <= 249; i++) {
+                    orders.push({
+                        id: v4(),
+                        customerId: customers[i].id,
+                        planId: plans[i].id, 
+                        referrer: referrers[getRandomInt(5)],
+                        consumption: [1600, 1600, 1600, 1600, 2400, 2400, 2400, 3200, 4000, 4500].reduce((a, c, i, o) => { return o[Math.floor(Math.random() * Math.floor(o.length))]; }),
+                        is_active: true,
+                        createdAt: customers[i].createdAt,
+                        updatedAt: timeFunc.randomTime(customers[i].createdAt, timeFunc.endTime),
+                        terminatedAt: null,
+                    });
+                }
             
-        /*await queryInterface.sequelize.query('SELECT Id FROM "Plans";', { type: queryInterface.sequelize.QueryTypes.SELECT })
-        .then(function(plans) { */
-        
-            for (let i = 0; i <= 249; i++) {
-                orders.push({
-                    id: v4(),
-                    customerId: customers[i].id,
-                    planId: plans[i].id, 
-                    referrer: referrers[getRandomInt(5)],
-                    consumption: [1600, 1600, 1600, 1600, 2400, 2400, 2400, 3200, 4000, 4500].reduce((a, c, i, o) => { return o[Math.floor(Math.random() * Math.floor(o.length))]; }),
-                    is_active: true,
-                    createdAt: customers[i].createdAt,
-                    updatedAt: timeFunc.randomTime(customers[i].createdAt, timeFunc.endTime),
-                    terminatedAt: null,
-                });
-            }
-        
-            //65 (25%) Customer haben eine und eine gecancelte Order
-            for (let i = 0; i <= 64; i++) {
-                const o =  timeFunc.randomTime(customers[i].createdAt, timeFunc.endTime);
-                const ou = timeFunc.randomTime(o, timeFunc.endTime);
-                orders.push({
-                    id: v4(),
-                    customerId: customers[i].id,
-                    planId: plans[i].id, 
-                    referrer: referrers[getRandomInt(5)],
-                    consumption: [1600, 1600, 1600, 1600, 2400, 2400, 2400, 3200, 4000, 4500].reduce((a, c, i, o) => { return o[Math.floor(Math.random() * Math.floor(o.length))]; }),
-                    is_active: false,
-                    createdAt: o,
-                    updatedAt: ou,
-                    terminatedAt: ou,
-                });
-            }
+                //65 (25%) Customer haben eine und eine gecancelte Order
+                for (let i = 0; i <= 64; i++) {
+                    const o =  timeFunc.randomTime(customers[i].createdAt, timeFunc.endTime);
+                    const ou = timeFunc.randomTime(o, timeFunc.endTime);
+                    orders.push({
+                        id: v4(),
+                        customerId: customers[i].id,
+                        planId: plans[i].id, 
+                        referrer: referrers[getRandomInt(5)],
+                        consumption: [1600, 1600, 1600, 1600, 2400, 2400, 2400, 3200, 4000, 4500].reduce((a, c, i, o) => { return o[Math.floor(Math.random() * Math.floor(o.length))]; }),
+                        is_active: false,
+                        createdAt: o,
+                        updatedAt: ou,
+                        terminatedAt: ou,
+                    });
+                }
 
-            //10 (5%) haben 2 Orders
-            for (let i = 65; i <= 74; i++) {
-                const o =  timeFunc.randomTime(customers[i].createdAt, timeFunc.endTime);
-                orders.push({
-                    id: v4(),
-                    customerId: customers[i].id,
-                    planId: plans[i].id, 
-                    referrer: referrers[getRandomInt(5)],
-                    consumption: [1600, 1600, 1600, 1600, 2400, 2400, 2400, 3200, 4000, 4500].reduce((a, c, i, o) => { return o[Math.floor(Math.random() * Math.floor(o.length))]; }),
-                    is_active: true,
-                    createdAt: o,
-                    updatedAt: timeFunc.randomTime(o, timeFunc.endTime),
-                    terminatedAt: null,
-                });
-            }
-        })
+                //10 (5%) haben 2 Orders
+                for (let i = 65; i <= 74; i++) {
+                    const o =  timeFunc.randomTime(customers[i].createdAt, timeFunc.endTime);
+                    orders.push({
+                        id: v4(),
+                        customerId: customers[i].id,
+                        planId: plans[i].id, 
+                        referrer: referrers[getRandomInt(5)],
+                        consumption: [1600, 1600, 1600, 1600, 2400, 2400, 2400, 3200, 4000, 4500].reduce((a, c, i, o) => { return o[Math.floor(Math.random() * Math.floor(o.length))]; }),
+                        is_active: true,
+                        createdAt: o,
+                        updatedAt: timeFunc.randomTime(o, timeFunc.endTime),
+                        terminatedAt: null,
+                    });
+                }
+            })
         return queryInterface.bulkInsert('Orders', orders);
     },
 
