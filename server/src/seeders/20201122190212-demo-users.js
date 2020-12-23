359401961d4858fd625206f8fcb194ac456a8254
LisaFremdt <56634686+LisaFremdt@users.noreply.github.com>
2020-12-23 21:29:28 +0100

    Update server/src/seeders/20201122190212-demo-users.js
    Co-authored-by: Tomas Kostadinov <github@kostadinov.it>

--- server/src/seeders/20201122190212-demo-users.js
+++ server/src/seeders/20201122190212-demo-users.js
@@ -8,7 +8,7 @@ module.exports = {
     up: async (queryInterface, Sequelize) => {
         let users = [];
         for (let i = 0; i <= 199; i++) {
-            const u = timeFunc.randomTime(timeFunc.startTime, timeFunc.endTime);
+            const date = timeFunc.randomTime(timeFunc.startTime, timeFunc.endTime);
             const SALT_FACTOR = 10;
             const hashedPassword = await bcrypt.hash(faker.internet.password(), SALT_FACTOR);
             users.push({
@@ -16,8 +16,8 @@ module.exports = {
                 email: faker.internet.email(),
                 password: hashedPassword,
                 is_admin: false,
-                createdAt: u,
-                updatedAt: timeFunc.randomTime(u, timeFunc.endTime)
+                createdAt: date,
+                updatedAt: timeFunc.randomTime(date, timeFunc.endTime)
             });
         }
         await queryInterface.bulkInsert('Users', users);
