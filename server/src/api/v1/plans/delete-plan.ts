5b8d16e3e5e0a8a7143f2d44c2748079c225454a
Dominik Ruf <aquanik.ruf@gmail.com>
2021-01-21 21:36:33 +0100

    FIX: delete check in deletePlan, if it has active orders. not necessary!

--- server/src/api/v1/plans/delete-plan.ts
+++ server/src/api/v1/plans/delete-plan.ts
@@ -1,35 +1,18 @@
 import {Request, Response} from 'express';
 import {wrapResponse} from '../../../functions/response-wrapper';
-import {Order} from '../../../models/order.model';
 import {Plan} from '../../../models/plan.model';
 
 /**
- *
- * @param req
- * @param res
+ * (Soft) deletes a plan with a given id from request
+ * 
+ * @param {Request} req
+ * @param {Reponse} res
+ * @returns {Promise<Response>}
  */
 export async function deletePlan(req: Request, res: Response): Promise<Response> {
     let success = true;
 
-    //check if unterminated orders exist with given planId
-    const activeOrders: Order | null = await Order.findOne(
-        {
-            where: {
-                planId: req.params.id,
-                terminatedAt: null
-            }
-        })
-        .catch(() => {
-            success = false;
-            return null;
-        });
-    if (!success) {
-        return res.status(500).send(wrapResponse(false, {error: 'Database error'}));
-    }
-    if (activeOrders !== null) {
-        return res.status(400).send(wrapResponse(false, {error: 'The given planId has active Orders and can not be deleted'}));
-    }
-
+    // A plan can always be set to inactive, even if it has active orders
     const updateResult = await Plan.update(
         {
             is_active: false
